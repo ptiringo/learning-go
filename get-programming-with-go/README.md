b127e386214a54ab95bbe079c98c96bce1c28217
ptiringo <ptiringo@gmail.com>
2020-01-01 19:15:59 +0900

    Done lesson 1

---
+++ get-programming-with-go/README.md
@@ -0,0 +1,3 @@
+# 入門 GO プログラミング
+
+- [入門Goプログラミング（NathanYoungman RogerPeppé 吉川邦夫）｜翔泳社の本](https://www.shoeisha.co.jp/book/detail/9784798158655)
---
+++ get-programming-with-go/lesson1/playground.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, ptiringo")
+	fmt.Println("Hello, world")
+	fmt.Println("Hello, 世界")
+	fmt.Println("Hello, 天地")
+	fmt.Println("Hello, мир")
+	fmt.Println("Hello, Mundo")
+}
