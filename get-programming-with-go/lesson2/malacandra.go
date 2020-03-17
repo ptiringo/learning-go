2acdc76e6d3a8eeee0e0dbdf9a3982300dc887e8
ptiringo <ptiringo@gmail.com>
2020-03-17 16:57:36 +0900

    refactor

--- get-programming-with-go/lesson2/malacandra.go
+++ get-programming-with-go/lesson2/malacandra.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	const days = 28
 	const distance = 56000000 // km
 
+	const days = 28
 	const hours = days * 24
 
 	fmt.Printf("%v km/時", distance / hours)
