0682c51df1719ff2191a6fa199e3fb46243bce21
Matsui <ptiringo@gmail.com>
2021-05-04 21:42:46 +0900

    lesson4

--- get-programming-with-go/README.md
+++ get-programming-with-go/README.md
@@ -1,3 +1,5 @@
 # 入門 GO プログラミング
 
 - [入門Goプログラミング（NathanYoungman RogerPeppé 吉川邦夫）｜翔泳社の本](https://www.shoeisha.co.jp/book/detail/9784798158655)
+- [nathany/get-programming-with-go: Source code listings for Get Programming with Go, a beginner's guide to the Go programming language.](https://github.com/nathany/get-programming-with-go)
+  - 練習問題回答
---
+++ get-programming-with-go/lesson4/random-dates.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+var era = "AD"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		year := 2018 + rand.Intn(10)
+		month := rand.Intn(12) + 1
+		daysInMonth := 31
+		switch month {
+		case 2:
+			if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
+				daysInMonth = 29
+			} else {
+				daysInMonth = 28
+			}
+		case 4, 6, 9, 11:
+			daysInMonth = 30
+		}
+		day := rand.Intn(daysInMonth) + 1
+		fmt.Println(era, year, month, day)
+	}
+}
